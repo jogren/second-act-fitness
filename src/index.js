0db3de4876e5cd049d89b459edfa5edaab1b185b
Jacob Ogren <jogren0044@gmail.com>
2020-01-15 07:15:41 -0700

    Import and implement BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 import './styles/main.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 
 serviceWorker.unregister();
