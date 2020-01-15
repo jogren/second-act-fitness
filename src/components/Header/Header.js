08436633b36f802f48681b21ed209a48b4da5504
Jacob Ogren <jogren0044@gmail.com>
2020-01-15 07:18:12 -0700

    Add Header component with background image

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import fitnessImg from '../../images/ElderlyArtVector.jpg';
+
+export const Header = () => {
+
+  return (
+    <header>
+      <img src={fitnessImg} alt="Fitness Silhouette"/>
+    </header>
+  )
+}
