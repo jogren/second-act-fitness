482bed8d15d6bc1edd1845e34badbba73356bb70
Jacob Ogren <jogren0044@gmail.com>
2020-01-15 07:16:31 -0700

    Add About files and include basic images for the page

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import runningImg from '../../images/running.jpg';
+import bikingImg from '../../images/biking.jpg';
+
+export const About = () => {
+
+  return (
+    <section className="About-container">
+      <h2>About Us</h2>
+      <div className="About_img-container">
+        <img src={runningImg} alt="About running profile"/>
+        <img src={bikingImg} alt="About biking profile" />
+      </div>
+    </section>
+  )
+}
