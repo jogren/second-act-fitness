c919b88b367c72795bb94ddd0d507e6fd64f3c2b
Jacob Ogren <jogren0044@gmail.com>
2020-01-16 16:12:08 -0700

    Update routes and what each one renders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Nav } from '../Nav/Nav';
-// import { Header } from '../Header/Header';
+import { Header } from '../Header/Header';
 import { About } from '../About/About';
 import { Book } from '../Book/Book';
 import { Login } from '../Login/Login';
+import { HomePage } from '../HomePage/HomePage';
 import { Route } from 'react-router-dom';
 
 
@@ -18,10 +19,31 @@ class App extends Component {
   render() {
     return (
       <main className="App">
-        <Nav />
-        <Route path="/about" component={ About } />
-        <Route path="/book-online" component={ Book } />
-        <Route path="/admin-login" component={ Login } />
+        <Route exact path="/" render={() =>
+          <section className="section_home">
+            <Nav />
+            <Header />
+            <HomePage />
+          </section>
+        } />
+        <Route path="/about" render={() =>
+          <section>
+            <Nav />
+            <About /> 
+          </section>
+        } />
+        <Route path="/book-online" render={() => 
+          <section>
+            <Nav />
+            <Book />
+          </section>
+        } />
+        <Route path="/admin-login" render={() =>
+          <section>
+            <Nav /> 
+            <Login /> 
+          </section>
+        } />
       </main>
     );
   }
