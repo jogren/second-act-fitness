5b26c88a21840d60040c070d30eee27936d19920
Jacob Ogren <jogren0044@gmail.com>
2020-01-17 11:09:14 -0700

    Update formatting of Book page

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -8,7 +8,8 @@ export class Book extends Component {
     this.state = {
       date: new Date(),
       time: '',
-      selectNewDate: false
+      view: 'calendar',
+      isBookEnabled: false
     }
   }
 
@@ -21,7 +22,20 @@ export class Book extends Component {
     this.setState({ time })
   }
 
-  render() {
+  changeView = () => {
+    const { view } = this.state;
+    if(view === 'calendar') {
+      this.setState({ view: 'booking' });
+    } else {
+      this.setState({ view: 'calendar' });
+    }
+  }
+
+  toggleBookEnabled = () => {
+    this.setState({ isBookEnabled: this.state.isBookEnabled ? false : true });
+  }
+
+  handleBookingWindow = () => {
     const { date, time } = this.state;
     let dayObj = {
       0: 'Sunday',
@@ -32,29 +46,50 @@ export class Book extends Component {
       5: 'Friday',
       6: 'Saturday'
     }
+    if(date && time) {
+      return <p>{dayObj[date.getDay()]}, {date.getMonth() + 1}/{date.getDate()}/{date.getFullYear()} | {time}</p>
+    } else if(date) {
+      return <p>{dayObj[date.getDay()]}, {date.getMonth() + 1}/{date.getDate()}/{date.getFullYear()}</p>
+    } else {
+      return <p></p>
+    }
+  }
+
+  render() {
+    const { date, time, view, isBookEnabled } = this.state;
     let timeOptions = ['6:00am', '7:00am', '8:00am', '9:00am', '6:00pm', '7:00pm', '9:00pm'];
     let inputList = timeOptions.map((option, index) => {
       let borderClass = option === time ? 'time-item-selected' : 'time-item-not-selected'
       return <button onClick={() => this.handleSelectTime(option)} className={borderClass} key={index}>{option}</button>
     })
+
     return (
       <section className="Book_section">
-        <h2>Book Online</h2>
+        <h2>Schedule Online</h2>
         <section className="calendar-container">
-          <Calendar
-            onChange={this.handleSelectDate}
-            value={this.state.date}
-          />
-          <div className="pick-appo-list">
-            <div className="appo-list-container">
-              {inputList}
-            </div>
-          </div>
+          { view === 'booking' ? 
+            <BookingForm changeView={this.changeView} toggleBookEnabled={this.toggleBookEnabled}/> 
+          : 
+            <article className="article_calendar-time">
+              <Calendar
+                onChange={this.handleSelectDate}
+                value={date}
+              />
+              <div className="pick-appo-list">
+                <div className="appo-list-container">
+                  {inputList}
+                </div>
+              </div> 
+            </article> 
+          }
         </section>
-        { date && time && <div className="booking-container">
-          <p>Please confirm your appointment for {dayObj[date.getDay()]}, {date.getMonth() + 1}/{date.getDate()}/{ date.getFullYear() } at { time }</p>
-          <BookingForm />
-        </div> }
+        <section className="booking-container">
+          <h3>In-Home Session</h3>
+          <p>1 hr | $25</p>
+          { this.handleBookingWindow() }
+          { view === 'calendar' && <button onClick={this.changeView} disabled={!time || !date}>Next</button> }
+          {view === 'booking' && <button disabled={!isBookEnabled}>Book</button> }
+        </section>
       </section>
     )
   }
