2bb21903b53151adb8239866deb4c13c12b295f6
Jacob Ogren <jogren0044@gmail.com>
2020-01-21 13:53:55 -0600

    Update page once admin logs in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,8 @@ export class Login extends Component {
     super();
     this.state = {
       loginEmail: '',
-      loginPassword: ''
+      loginPassword: '',
+      isLoggedIn: false
     }
   }
 
@@ -14,41 +15,50 @@ export class Login extends Component {
   }
 
   submitLogin = (e) => {
-    console.log('test')
+    e.preventDefault();
+    this.setState({ loginEmail: '', loginPassword: '' })
+    this.setState({ isLoggedIn: true })
   }
 
   render() {
-    const { loginEmail, loginPassword } = this.state;
+    const { loginEmail, loginPassword, isLoggedIn } = this.state;
     return (
-      <section className="Login_section">
-        <article className="login-container">
-          <h2>Login</h2>
-          <form className="login-form">
-            <label>Email
-              <input
-                className="login-email"
-                type="text"
-                name="loginEmail"
-                value={loginEmail}
-                onChange={(e) => this.handleChange(e)}
-                autoComplete="username"
-              />
-            </label>
-            <label>Password
-              <input
-                className="login-password"
-                type="password"
-                name="loginPassword"
-                value={loginPassword}
-                onChange={(e) => this.handleChange(e)}
-                autoComplete="current-password"
-              />
-            </label>
-            <button className="login-button" disabled={!loginEmail || !loginPassword} onClick={(e) => this.submitLogin(e)}>Submit</button>
-            {/* {this.props.hasErroredReducer && <p>Please enter a valid email and password</p>} */}
-          </form>
-        </article>
-      </section>
+        <section className="Login_section">
+          { !isLoggedIn ?
+          <article className="login-container">
+            <h2>Login</h2>
+            <form className="login-form">
+              <label>Email
+                <input
+                  className="login-email"
+                  type="text"
+                  name="loginEmail"
+                  value={loginEmail}
+                  onChange={(e) => this.handleChange(e)}
+                  autoComplete="username"
+                />
+              </label>
+              <label>Password
+                <input
+                  className="login-password"
+                  type="password"
+                  name="loginPassword"
+                  value={loginPassword}
+                  onChange={(e) => this.handleChange(e)}
+                  autoComplete="current-password"
+                />
+              </label>
+              <button className="login-button" disabled={!loginEmail || !loginPassword} onClick={(e) => this.submitLogin(e)}>Submit</button>
+              {/* {this.props.hasErroredReducer && <p>Please enter a valid email and password</p>} */}
+            </form>
+          </article>
+          : 
+          <article className="article_admin-logged-in">
+            <button>Settings</button>
+            <button>Calendar View</button>
+          </article>
+        }
+        </section>
     )
   }
 }
